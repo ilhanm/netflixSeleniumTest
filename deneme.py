f2035020f299e3388eb5be6565234b7e0cf91ead
ilanasusual <ilhanmertalan@gmail.com>
2021-11-19 21:00:58 +0300

    this file is not used in program, so commented.

--- deneme.py
+++ deneme.py
@@ -1,4 +1,4 @@
-sourceFile = open('demo.txt', 'w')
-print('Hello, Python!', file = sourceFile)
-print('Hello, Python2!', file = sourceFile)
-sourceFile.close()
+# sourceFile = open('demo.txt', 'w')
+# print('Hello, Python!', file = sourceFile)
+# print('Hello, Python2!', file = sourceFile)
+# sourceFile.close()
